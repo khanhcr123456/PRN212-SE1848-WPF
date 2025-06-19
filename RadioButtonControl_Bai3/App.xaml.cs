617de809e2f217abe0951994a47b69132817a1f6
Huỳnh Đức Khánh <khanhhdse180719@fpt.edu.vn>
2025-06-19 20:45:07 +0700

    Bài Tập WPF Bài 3
    Bài Tập WPF Bài 3

---
+++ RadioButtonControl_Bai3/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RadioButtonControl_Bai3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ RadioButtonControl_Bai3/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioButtonControl_Bai3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGui_Click(object sender, RoutedEventArgs e)
+        {
+            string binhChon = "";
+            if (radRatTot.IsChecked == true)
+                binhChon = radRatTot.Content + "";
+            else if (radTot.IsChecked == true)
+                binhChon = radTot.Content + "";
+            else if (radTamDuoc.IsChecked == true)
+                binhChon = radTamDuoc.Content + "";
+            else if (radKhongTot.IsChecked == true)
+                binhChon = radKhongTot.Content + "";
+            string gioiTinh = "";
+            if (radNam.IsChecked == true)
+                gioiTinh = radNam.Content + "";
+            else if (radNu.IsChecked == true)
+                gioiTinh = radNu.Content + "";
+            string infor = "Bạn bình chọn Hệ Thống=" + binhChon + Environment.NewLine;
+            infor += "Giới tính của bạn=" + gioiTinh;
+            MessageBoxResult ret;
+            ret = MessageBox.Show(infor, "Mời bạn xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (ret == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Cảm ơn bạn đã bình chọn");
+            }
+        }
+
+        private void btnHuy_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
